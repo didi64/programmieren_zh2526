ebb75fcfca7dfc9e7b1630f55423be56a7ef401a
Dieter Probst <dieter.probst@gmail.com>
2026-02-15 02:09:38 +0100

    .

--- notebooks/L13/Spielsteine_verschieben/schach_darstellung.py
+++ notebooks/L13/Spielsteine_verschieben/schach_darstellung.py
@@ -8,14 +8,14 @@ def draw_chessboard(canvas):
     H.draw_board(canvas, BOARD_SPEC)
 
 
-def place_pieces(canvas, pps):
+def apply_changes(canvas, changes):
     x0, y0, dx, dy, ncol, nrow = BOARD_SPEC
     canvas.font = f'{dx}px sans-serif'
     canvas.text_align = 'center'
     canvas.text_baseline = 'ideographic'
-    for piece, col, row in pps:
+    for symbol, col, row in changes:
         canvas.clear_rect(x0+col*dx, y0+row*dy, dx, dy)
-        canvas.fill_text(piece, x0+(col+0.5)*dx, y0+(row+1)*dy)
+        canvas.fill_text(symbol, x0+(col+0.5)*dx, y0+(row+1)*dy)
 
 
 def update(canvas, event, **kwargs):
