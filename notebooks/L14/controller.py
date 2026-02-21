ec67fd3062443204a4a01170e3727845b168c3cf
Dieter Probst <dieter.probst@gmail.com>
2026-02-21 16:36:10 +0100

    .

--- notebooks/L14/controller.py
+++ notebooks/L14/controller.py
@@ -1,47 +1,38 @@
-# import schach2 as schach
-from schach3 import Schach
-# import schach_D as D
-from schach_D2 import View
 import helpers as H
-from ipycanvas import MultiCanvas
-from ipywidgets import Output, Button
+from ipywidgets import Output
 from IPython.display import display
 
-schach = Schach()
-view = View()
 
-state = {'mouse_down': None}
+class Controller:
+    layout = {'border': '1px solid black'}
+    # out = Output(layout=layout)
+    
+    def __init__(self, schach, view):
+       self.schach = schach
+       self.view = view
+       self.mouse_down = None 
 
-layout = {'border': '1px solid black'}
-out = Output(layout=layout)
-button = Button(description='New Game')
-# mcanvas = MultiCanvas(2, width=200, height=200, layout=layout)
-# bg, fg = mcanvas
+       self.view.draw_chessboard(view.bg)
+       self.schach.update = lambda event, **kwargs: self.view.update(self.view.fg, event, **kwargs)
+       self.schach.new_game()
 
+       self.view.mcanvas.on_mouse_down(self.on_mouse_down)
+       self.view.mcanvas.on_mouse_up(self.on_mouse_up)
+       self.view.button.on_click(lambda _: self.schach.new_game())  # Argument ignorieren
 
-@out.capture(clear_output=True)
-def on_mouse_down(x, y):
-    state['mouse_down'] = (x, y)
-    print(f'A (state[\'mouse_down\']) set to ({int(x)}, {int(y)})')
-
-
-@out.capture()
-def on_mouse_up(x, y):
-    target = H.xy2cr(x, y, view.BOARD_SPEC)
-    src = H.xy2cr(*state['mouse_down'], view.BOARD_SPEC)
-    schach.raw_move(src, target)
-    print(f'move from {src} to {target}')
+    # @out.capture(clear_output=True)
+    def on_mouse_down(self, x, y):
+        self.mouse_down = (x, y)
+        # print(f'A (state[\'mouse_down\']) set to ({int(x)}, {int(y)})')
 
 
-view.draw_chessboard(view.bg)
+    # @out.capture()
+    def on_mouse_up(self, x, y):
+        target = H.xy2cr(x, y, self.view.BOARD_SPEC)
+        src = H.xy2cr(*self.mouse_down, self.view.BOARD_SPEC)
+        self.schach.raw_move(src, target)
+        # print(f'move from {src} to {target}')
 
-schach.update = lambda event, **kwargs: view.update(view.fg, event, **kwargs)
-schach.new_game()
-
-view.mcanvas.on_mouse_down(on_mouse_down)
-view.mcanvas.on_mouse_up(on_mouse_up)
-
-
-view.button.on_click(lambda _: schach.new_game())  # Argument ignorieren
-
-display(view.mcanvas, view.button, out)
+    def _ipython_display_(self):
+        # display(self.view.mcanvas, self.view.button, self.out)
+        display(self.view.mcanvas, self.view.button)
