280d2022821c65c65f80daf1b7fceb8f3c871794
Dieter Probst <dieter.probst@gmail.com>
2026-02-22 10:55:21 +0100

    .

--- notebooks/L14/Klassen/foo.py
+++ notebooks/L14/Klassen/foo.py
@@ -1,16 +1,21 @@
-print('Erste Zeile der Klasse Foo')
+print('Erste Zeile des Moduls foo')
 x = 41
 x = x + 1  # x erhoehen
+y = 10
 
-def f(*args):
-    print(f'f bekommt {len(args)} Argument(e):')
-    print(*args, sep=' und ')
 
-def g():
-    x = 1  # lokale Variable der Funktion
-    Foo.x = 10 * Foo.x  # Im Klassenbody definierte Variable x
-    print(f'Im Klassenbody definierte Variable x: {Foo.x}')
-    print(f'Im Funktionsbody von g definierte Variable x: {x}')
+def g_1():
+    x = 1  # lokale Variable der Funktion, ueberdeckt (shadows) globales x
+    # foo.x = 10 * foo.x  macht keinen Sinn, Modul kennt seinen Namen nicht!
+    print(f'Im  Modul definierte Variable y: {y}')
+    print(f'Im Funktionsbody von g_1 definierte Variable x: {x}')
+
+
+def g_2():
+    global x  # x wird als global deklariert
+    x = 10 * x  # globales x wird modifiziert
+    print(f'Im Modul definierte Variable x: {x}')
+
 
 print(x)
-print('Letzte Zeile der Klasse Foo')
+print('Letzte Zeile de Moduls foo')
