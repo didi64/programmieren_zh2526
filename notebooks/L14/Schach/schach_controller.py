f4c0f583a0ebf26626974ceddb41cac7b5f0c45d
Dieter Probst <dieter.probst@gmail.com>
2026-02-23 12:50:25 +0100

    .

--- notebooks/L14/Schach/schach_controller.py
+++ notebooks/L14/Schach/schach_controller.py
@@ -1,30 +1,29 @@
 import helpers as H
-from ipywidgets import Output
+# from ipywidgets import Output
 from IPython.display import display
 
 
 class Controller:
     layout = {'border': '1px solid black'}
     # out = Output(layout=layout)
-    
+
     def __init__(self, schach, view):
-       self.schach = schach
-       self.view = view
-       self.mouse_down = None 
+        self.schach = schach
+        self.view = view
+        self.mouse_down = None
 
-       self.view.draw_chessboard(view.bg)
-       self.schach.update = lambda event, **kwargs: self.view.update(self.view.fg, event, **kwargs)
-       self.schach.new_game()
+        self.view.draw_chessboard(view.bg)
+        self.schach.update = lambda event, **kwargs: self.view.update(self.view.fg, event, **kwargs)
+        self.schach.new_game()
 
-       self.view.mcanvas.on_mouse_down(self.on_mouse_down)
-       self.view.mcanvas.on_mouse_up(self.on_mouse_up)
-       self.view.button.on_click(lambda _: self.schach.new_game())  # Argument ignorieren
+        self.view.mcanvas.on_mouse_down(self.on_mouse_down)
+        self.view.mcanvas.on_mouse_up(self.on_mouse_up)
+        self.view.button.on_click(lambda _: self.schach.new_game())  # Argument ignorieren
 
     # @out.capture(clear_output=True)
     def on_mouse_down(self, x, y):
         self.mouse_down = (x, y)
         # print(f'A (state[\'mouse_down\']) set to ({int(x)}, {int(y)})')
-
 
     # @out.capture()
     def on_mouse_up(self, x, y):
