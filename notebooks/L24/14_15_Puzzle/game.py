a164168a77fd0262b3567092745c6d2fb1daa8ab
Dieter Probst <dieter.probst@gmail.com>
2026-06-05 13:44:16 +0200

    .

--- notebooks/L24/14_15_Puzzle/game.py
+++ notebooks/L24/14_15_Puzzle/game.py
@@ -1,8 +1,15 @@
+import matrix_helpers as M
+
+
 board = []
 
 
 def update(event):
     print(event)
+
+
+def sign(x):
+    return (x > 0) - (x < 0)
 
 
 def new_game():
@@ -21,3 +28,26 @@ def move(pos):
             board[y][x] = 0
             board[ny][nx] = tmp
             update('move')
+
+
+def move_1(pos):
+    if M.get_item(board, pos) == 0:
+        return
+
+    x, y = pos
+    xh, yh = M.find(board, 0)
+    if x == xh:
+        dist = y - yh
+        s = sign(dist)
+        for i in range(abs(dist)):
+            M.swap(board, (xh, yh+s*i), (xh, yh+s*(i+1)))
+
+    if y == yh:
+        dist = x - xh
+        s = sign(dist)
+        for i in range(abs(dist)):
+            M.swap(board, (xh+s*i, yh), (xh+s*(i+1), yh))
+
+    update('move1')
+
+
--- notebooks/L24/sudoku_solver.py
+++ notebooks/L24/sudoku_solver.py
@@ -9,6 +9,17 @@ def search_df(node, get_neighbors):
         nodes_to_visit.extend(get_neighbors(node))
 
 
+def search_df_all(node, get_neighbors):
+    nodes_to_visit = [node]
+
+    while nodes_to_visit:
+        node = nodes_to_visit.pop()
+        if 0 not in node:
+            yield node
+
+        nodes_to_visit.extend(get_neighbors(node))
+
+
 def get_row(sudoku, row):
     return (n for i in range(9)
             if (n := sudoku[9*row+i]) > 0)
@@ -36,6 +47,7 @@ def are_unique(items):
 
 
 def is_sudoku(s):
+    '''testet, ob der String s ein Sudoku ist'''
     if len(s) != 81 or any(not c.isdigit() for c in s):
         return False
 
@@ -45,6 +57,43 @@ def is_sudoku(s):
             if not are_unique(get_group(sudoku, i)):
                 return False
     return True
+
+
+def get_options(sudoku, i):
+    if 0 != sudoku[i]:
+        return ()
+
+    options = []
+    row, col = divmod(i, 9)
+    block = 3*(row//3) + col//3
+    for number in range(1, 10):
+        if not (number in get_row(sudoku, row)
+                or number in get_col(sudoku, col)
+                or number in get_block(sudoku, block)):
+            options.append(number)
+
+    return options
+
+
+def get_good_neighbors(sudoku):
+    best_opts = (0, 10, None)  # index, n_opts, opts
+
+    for i, n in enumerate(sudoku):
+        if n != 0:
+            continue
+
+        opts = get_options(sudoku, i)
+        n_opts = len(opts)
+        if n_opts < best_opts[1]:
+            best_opts = (i, n_opts, opts)
+        if n_opts == 1:
+            break
+
+    i, _, numbers = best_opts
+    sudoku = list(sudoku)
+    for number in numbers:
+        sudoku[i] = number
+        yield tuple(sudoku)
 
 
 def get_neighbors(sudoku):
@@ -63,11 +112,19 @@ def get_neighbors(sudoku):
         yield tuple(sudoku)
 
 
-def solve_sudoku(s):
-    sudoku = tuple(int(c) for c in s)
-    if not is_sudoku(sudoku):
+def solve_sudoku(s, good_neighbors=False):
+    if not is_sudoku(s):
         print('kein Sudoku!')
         return
 
-    solution = search_df(sudoku, get_neighbors)
+    get_succ = get_good_neighbors if good_neighbors else get_neighbors
+
+    sudoku = tuple(int(c) for c in s)
+    solution = search_df(sudoku, get_succ)
     return ''.join(str(n) for n in solution)
+
+
+def print_sudoku_str(s):
+    s = s.replace('0', '.')
+    s = '\n'.join(s[9*i:9*i+9] for i in range(9))
+    print(s)
