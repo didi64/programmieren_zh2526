a64736a129a12f7a19cd5de46eb939c45b11c00a
Dieter Probst <dieter.probst@gmail.com>
2026-06-05 17:31:59 +0200

    .

--- notebooks/L24/14_15_Puzzle/game.py
+++ notebooks/L24/14_15_Puzzle/game.py
@@ -28,26 +28,3 @@ def move(pos):
             board[y][x] = 0
             board[ny][nx] = tmp
             update('move')
-
-
-def move_1(pos):
-    if M.get_item(board, pos) == 0:
-        return
-
-    x, y = pos
-    xh, yh = M.find(board, 0)
-    if x == xh:
-        dist = y - yh
-        s = sign(dist)
-        for i in range(abs(dist)):
-            M.swap(board, (xh, yh+s*i), (xh, yh+s*(i+1)))
-
-    if y == yh:
-        dist = x - xh
-        s = sign(dist)
-        for i in range(abs(dist)):
-            M.swap(board, (xh+s*i, yh), (xh+s*(i+1), yh))
-
-    update('move1')
-
-
